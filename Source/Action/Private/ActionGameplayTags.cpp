68be4df3ccb27986a119272690118a5a172ce890
LingLeiLuo <1140150633@qq.com>
2026-03-06 22:50:05 +0800

    增加GameplayTags类

---
+++ Source/Action/Private/ActionGameplayTags.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ActionGameplayTags.h"
+
+namespace ActionGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "Input.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "Input.Look");
+}
