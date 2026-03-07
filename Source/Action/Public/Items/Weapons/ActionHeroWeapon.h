0a167e2fcd942a3873ba163d95b7d9b43090ae5e
LingLeiLuo <1140150633@qq.com>
2026-03-07 12:25:00 +0800

    :hammer: 添加英雄武器基类和实现

---
+++ Source/Action/Public/Items/Weapons/ActionHeroWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Weapons/ActionWeaponBase.h"
+#include "ActionHeroWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTION_API AActionHeroWeapon : public AActionWeaponBase
+{
+	GENERATED_BODY()
+	
+};
