19e69f67b9932b63bc5ac700b657359c418810f8
soliszha <1140150633@qq.com>
2026-03-09 19:14:06 +0800

    feat: 添加 Gameplay Tag 操作工具函数

--- Source/Action/Private/ActionFunctionLibrary.cpp
+++ Source/Action/Private/ActionFunctionLibrary.cpp
@@ -3,3 +3,51 @@
 
 #include "ActionFunctionLibrary.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystem/ActionAbilitySystemComponent.h"
+
+UActionAbilitySystemComponent* UActionFunctionLibrary::NativeGetActionActionAbilitySystemComponentFromActor(AActor* InActor)
+{
+	check(InActor);
+
+	return CastChecked<UActionAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(InActor));
+}
+
+void UActionFunctionLibrary::AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag InGameplayTag)
+{
+	check(InActor);
+	check(InGameplayTag.IsValid());
+
+	UActionAbilitySystemComponent* AbilitySystemComponent = NativeGetActionActionAbilitySystemComponentFromActor(InActor);
+	if (!AbilitySystemComponent->HasMatchingGameplayTag(InGameplayTag))
+	{
+		AbilitySystemComponent->AddLooseGameplayTag(InGameplayTag);
+	}
+}
+
+void UActionFunctionLibrary::RemoveGameplayTagFromActorIfFound(AActor* InActor, FGameplayTag InGameplayTag)
+{
+	check(InActor);
+	check(InGameplayTag.IsValid());
+
+	UActionAbilitySystemComponent* AbilitySystemComponent = NativeGetActionActionAbilitySystemComponentFromActor(InActor);
+
+	if (AbilitySystemComponent->HasMatchingGameplayTag(InGameplayTag))
+	{
+		AbilitySystemComponent->RemoveLooseGameplayTag(InGameplayTag);
+	}
+}
+
+bool UActionFunctionLibrary::NativeDoesActorHaveTag(AActor* InActor, FGameplayTag InGameplayTag)
+{
+	UActionAbilitySystemComponent* AbilitySystemComponent = NativeGetActionActionAbilitySystemComponentFromActor(InActor);
+	
+	return AbilitySystemComponent->HasMatchingGameplayTag(InGameplayTag);
+}
+
+void UActionFunctionLibrary::K2_DoesActorHaveTag(AActor* InActor, FGameplayTag InGameplayTag,
+	EActionConfirmType& OutConfirmType)
+{
+	OutConfirmType = NativeDoesActorHaveTag(InActor, InGameplayTag) ? EActionConfirmType::Yes : EActionConfirmType::No;
+}
+
--- Source/Action/Public/ActionFunctionLibrary.h
+++ Source/Action/Public/ActionFunctionLibrary.h
@@ -6,12 +6,32 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "ActionFunctionLibrary.generated.h"
 
-/**
- * 
- */
+struct FGameplayTag;
+class UActionAbilitySystemComponent;
+
+UENUM()
+enum class EActionConfirmType : uint8
+{
+	Yes,
+	No
+};
+
 UCLASS()
 class ACTION_API UActionFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+
+public:
+	static UActionAbilitySystemComponent* NativeGetActionActionAbilitySystemComponentFromActor(AActor* InActor);
+
+	UFUNCTION(BlueprintCallable, Category = "Action|FunctionLibrary")
+	static void AddGameplayTagToActorIfNone(AActor* InActor, FGameplayTag InGameplayTag);
+
+	UFUNCTION(BlueprintCallable, Category = "Action|FunctionLibrary")
+	static void RemoveGameplayTagFromActorIfFound(AActor* InActor, FGameplayTag InGameplayTag);
+
+	static bool NativeDoesActorHaveTag(AActor* InActor, FGameplayTag InGameplayTag);
+
+	UFUNCTION(BlueprintCallable, Category = "Action|FunctionLibrary", meta=(DisplayName="Does Actor Have Tag", ExpandEnumAsExecs="OutConfirmType"))
+	static void K2_DoesActorHaveTag(AActor* InActor, FGameplayTag InGameplayTag, EActionConfirmType& OutConfirmType);
 };
