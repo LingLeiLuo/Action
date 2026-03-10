a6129c7c66704d8a19888e9f2883d4cad9a41b45
LingLeiLuo <1140150633@qq.com>
2026-03-11 01:11:57 +0800

    添加发送游戏事件动画通知的unlua脚本

---
+++ Content/Script/Action/Shared/AnimNotify/AnimNotify_SendGameplayEvent.lua
@@ -0,0 +1,22 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+
+---@type AN_SendGameplayEventToOwner_C
+local M = UnLua.Class()
+
+function M:Received_Notify(MeshComp, Animation)
+    local payload = UE.FGameplayEventData()
+
+    --我草了，蓝图属性默认带空格直接调不出来，还得self["  "]的形式
+    UE.UAbilitySystemBlueprintLibrary.SendGameplayEventToActor(MeshComp:GetOwner(), self["Event Tag"], payload)
+    print("Received_Notify")
+    
+    return true
+end
+
+return M
